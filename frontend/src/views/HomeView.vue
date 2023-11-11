30c03655ef7fdcd73e0d77d0d51c6a3eef2f8434
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-11-11 20:48:17 -0300

    login with google

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { GoogleLogin, decodeCredential } from 'vue3-google-login'
 import TheWelcome from '../components/TheWelcome.vue'
+
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+const callback = (response: { credential: string }) => {
+  const userData = decodeCredential(response.credential)
+  console.log(userData)
+}
 </script>
 
 <template>
   <main>
+    <GoogleLogin
+      :client-id="CLIENT_ID"
+      :callback="callback" 
+    />
     <TheWelcome />
   </main>
 </template>
