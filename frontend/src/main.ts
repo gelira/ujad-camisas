ecb88fd7064d5e5fc3b1e6752bf050623dcac755
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-11-14 11:54:24 -0300

    icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -8,6 +10,9 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
 })
 
 import App from './App.vue'
