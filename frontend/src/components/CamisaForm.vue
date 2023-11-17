0ec2ecda445f894847adf5786b3adf0a06b6d9a4
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-11-17 10:47:09 -0300

    update camisa

---
+++ frontend/src/components/CamisaForm.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+import { computed, onMounted, reactive, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import type { VForm } from 'vuetify/components/VForm'
+
+import { updateCamisa, type Camisa } from '@/api/camisa'
+import { fetchModelos, type Modelo } from '@/api/modelo'
+import { fetchTamanhos, type Tamanho } from '@/api/tamanho'
+
+interface State {
+  nomePessoa: string
+  modeloId: string | null
+  tamanhoId: string | null
+  totalPago: number
+  modelos: Modelo[]
+  tamanhos: Tamanho[]
+}
+
+const props = defineProps<{ camisa: Camisa | null, open: boolean }>()
+const emit = defineEmits<{ (e: 'close'): void, (e: 'saved'): void }>()
+const state = reactive<State>({
+  nomePessoa: '',
+  modeloId: null,
+  tamanhoId: null,
+  totalPago: 0,
+  modelos: [],
+  tamanhos: []
+})
+
+const form = ref<VForm>()
+
+const title = computed(
+  () => props.camisa ? 'Atualizar pedido de camisa' : 'Criar pedido de camisa'
+)
+
+const submit = async () => {
+  if (!form.value) {
+    return
+  }
+
+  try {
+    const { valid } = await form.value.validate()
+  
+    if (!valid) {
+      return
+    }
+  
+    if (props.camisa) {
+      await updateCamisa(props.camisa.id, {
+        nomePessoa: state.nomePessoa,
+        modeloId: state.modeloId as string,
+        tamanhoId: state.tamanhoId as string,
+        totalPago: state.totalPago
+      })
+    } else {
+      
+    }
+
+    emit('saved')
+  } catch {
+    // do nothing
+  }
+}
+
+watch(
+  () => props.camisa,
+  (value) => {
+    state.nomePessoa = value?.nomePessoa ?? ''
+    state.modeloId = value?.modeloId ?? null
+    state.tamanhoId = value?.tamanhoId ?? null
+  },
+  { immediate: true }
+)
+
+onMounted(() => {
+  fetchModelos()
+    .then(({ data }) => state.modelos = data.modelos)
+    .catch(() => state.modelos = [])
+
+  fetchTamanhos()
+    .then(({ data }) => state.tamanhos = data.tamanhos)
+    .catch(() => state.tamanhos = [])
+})
+</script>
+
+<template>
+  <v-dialog v-model="props.open" persistent>
+    <v-card>
+      <v-card-title>{{ title }}</v-card-title>
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field
+            v-model="state.nomePessoa"
+            label="Nome da pessoa"
+          ></v-text-field>
+
+          <v-select
+            v-model="state.modeloId"
+            :items="state.modelos"
+            item-value="id"
+            item-title="descricao"
+            label="Modelo"
+            :rules="[v => !!v || 'Campo obrigatório']"
+          ></v-select>
+
+          <v-select
+            v-model="state.tamanhoId"
+            :items="state.tamanhos"
+            item-value="id"
+            item-title="descricao"
+            label="Tamanho"
+            :rules="[v => !!v || 'Campo obrigatório']"
+          ></v-select>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn
+          variant="elevated"
+          color="primary"
+          @click="emit('close')"
+        >Cancelar</v-btn>
+        <v-btn
+          variant="elevated"
+          color="success"
+          @click="submit"
+        >Salvar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped>
+.v-card-actions {
+  justify-content: flex-end;
+}
+</style>
--- frontend/src/views/CamisasView.vue
+++ frontend/src/views/CamisasView.vue
@@ -5,11 +5,13 @@ import { useRoute } from 'vue-router'
 import { fetchCamisas, type Camisa } from '@/api/camisa'
 import CamisasTable from '@/components/CamisasTable.vue'
 import DeleteCamisa from '@/components/DeleteCamisa.vue'
+import CamisaForm from '@/components/CamisaForm.vue'
 
 interface State {
   camisas: Camisa[]
   camisaToEdit: Camisa | null
   camisaToDelete: Camisa | null
+  openForm: boolean
 }
 
 const route = useRoute()
@@ -17,8 +19,17 @@ const route = useRoute()
 const state = reactive<State>({
   camisas: [],
   camisaToEdit: null,
-  camisaToDelete: null
+  camisaToDelete: null,
+  openForm: false
 })
+
+const fetchCamisasAndSetState = () => {
+  const setorId = route.params.id
+
+  fetchCamisas(setorId)
+    .then(({ data }) => state.camisas = data.camisas)
+    .catch(() => state.camisas = [])
+}
 
 const findCamisaById = (id: string) => {
   return state.camisas.find(i => i.id === id) ?? null
@@ -29,22 +40,39 @@ const handleDeleted = (camisaId: string) => {
   state.camisaToDelete = null
 }
 
-watch(
-  () => route.params.id,
-  (setorId) => {
-    fetchCamisas(setorId)
-      .then(({ data }) => state.camisas = data.camisas)
-      .catch(() => state.camisas = [])
-  },
-  { immediate: true }
-)
+const selectToEdit = (camisaId: string) => {
+  state.camisaToEdit = findCamisaById(camisaId)
+  state.openForm = true
+}
+
+const selectToDelete = (camisaId: string) => {
+  state.camisaToDelete = findCamisaById(camisaId)
+} 
+
+const removeSelectToEdit = () => {
+  state.camisaToEdit = null
+  state.openForm = false
+}
+
+const handleSaved = () => {
+  fetchCamisasAndSetState()
+  removeSelectToEdit()
+}
+
+watch(() => route.params.id, fetchCamisasAndSetState, { immediate: true })
 </script>
 
 <template>
   <CamisasTable
     :camisas="state.camisas"
-    @edit="state.camisaToEdit = findCamisaById($event)"
-    @delete="state.camisaToDelete = findCamisaById($event)"
+    @edit="selectToEdit"
+    @delete="selectToDelete"
+  />
+  <CamisaForm
+    :camisa="state.camisaToEdit"
+    :open="state.openForm"
+    @close="removeSelectToEdit"
+    @saved="handleSaved"
   />
   <DeleteCamisa
     :camisa="state.camisaToDelete"
