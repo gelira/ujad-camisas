11fbe835cd72b56e547697a48e6f3f1201015833
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-11-18 15:50:02 -0300

    performance improvement

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,19 +1,10 @@
 import { createApp } from 'vue'
 
 import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
 
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-  },
-})
+const vuetify = createVuetify()
 
 import App from './App.vue'
 import router from './router'
