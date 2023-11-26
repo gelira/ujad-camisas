3840cc20487e126ecc514fdea1e999064d84733b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2023-11-26 18:25:14 -0300

    improvement for sidebar menu

--- frontend/src/components/AppBar.vue
+++ frontend/src/components/AppBar.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-import { computed, reactive } from 'vue'
+import { computed, reactive, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 import { useAuthStore } from '@/stores/auth'
 import { useSetorStore } from '@/stores/setor'
+
+interface State {
+  drawerOpen: boolean
+  menuOpen: string[]
+}
 
 const route = useRoute()
 const router = useRouter()
 const authStore = useAuthStore()
 const setorStore = useSetorStore()
 
-const state = reactive({ drawerOpen: false })
+const state = reactive<State>({
+  drawerOpen: false,
+  menuOpen: ['setores']
+})
 
 const title = computed(() => {
   if (!setorStore.setorSelected) {
@@ -20,6 +28,8 @@ const title = computed(() => {
 })
 
 const setorId = computed(() => route.params.id as string)
+
+const setorMenuOpen = computed(() => state.menuOpen.includes('setores'))
 
 const toggle = () => {
   state.drawerOpen = !state.drawerOpen
@@ -44,15 +54,22 @@ const navigate = (id: string) => {
     v-model="state.drawerOpen"
     temporary
   >
-    <v-list>
+    <v-list v-model:opened="state.menuOpen">
       <v-list-item
         append-icon="mdi-account-circle"
         :title="authStore.nome"
       />
       <v-divider />
-      <v-list-group>
+      <v-list-group
+        value="setores"
+        :class="['setores', { open: setorMenuOpen }]"
+      >
         <template v-slot:activator="{ props }">
-          <v-list-item v-bind="props" title="Setores" />
+          <v-list-item
+            v-bind="props"
+            title="Setores"
+            :variant="setorMenuOpen ? 'elevated' : 'text'"
+          />
         </template>
 
         <v-list-item
@@ -64,6 +81,34 @@ const navigate = (id: string) => {
         />
       </v-list-group>
       <v-divider />
+      <v-list-item
+        title="Sair"
+        append-icon="mdi-logout"
+        class="logout"
+      />
     </v-list>
   </v-navigation-drawer>
 </template>
+
+<style scoped lang="scss">
+.v-list {
+  height: calc(100dvh - 64px);
+}
+
+.setores {
+  max-height: calc(100% - 98px);
+
+  &.open {
+    height: 100%;
+  }
+
+  :deep(.v-list-group__items) {
+    max-height: calc(100% - 48px);
+    overflow: auto;
+  }
+}
+
+.logout {
+  color: red;
+}
+</style>
