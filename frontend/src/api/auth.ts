99d45f575c0a639d38ca05aa826c376b3ae8a9e2
Geraldo Eloi <geraldo.eloi11@gmail.com>
2025-12-13 21:44:14 -0300

    feat: new login view

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -7,3 +7,16 @@ export function apiValidateGoogleCredential(credential: string) {
 export function apiValidateAccessToken() {
   return apiClient().get<UserInfo>('/auth/user-info')
 }
+
+export function apiGetAuthCode(email: string) {
+  return apiClient(false).get<{ id: string }>('/auth/code', {
+    params: { email }
+  })
+}
+
+export function apiPostAuthCode(authCodeId: string, code: string) {
+  return apiClient(false).post<{ token: string }>('/auth/code', {
+    id: authCodeId,
+    code
+  })
+}
